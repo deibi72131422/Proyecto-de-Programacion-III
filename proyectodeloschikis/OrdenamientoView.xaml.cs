df5735286f6395db8dc80d802eeb4f4dfa29d8e6
Deibi <deibi6948@gmail.com>
2026-03-20 10:37:26 -0400

    Cambie los estilos

--- proyectodeloschikis/OrdenamientoView.xaml.cs
+++ proyectodeloschikis/OrdenamientoView.xaml.cs
@@ -20,9 +20,45 @@ namespace proyectodeloschikis
     /// </summary>
     public partial class OrdenamientoView : UserControl
     {
+        List<int> datos = new List<int>();
+
         public OrdenamientoView()
         {
             InitializeComponent();
+        }
+
+        private void Agregar_Click(object sender, RoutedEventArgs e)
+        {
+            int num;
+            if (int.TryParse(txtNumero.Text, out num))
+            {
+                datos.Add(num);
+                listaDatos.ItemsSource = null;
+                listaDatos.ItemsSource = datos;
+                txtNumero.Clear();
+            }
+        }
+
+        private void Ordenar_Click(object sender, RoutedEventArgs e)
+        {
+            datos = datos.OrderBy(x => x).ToList();
+
+            listaDatos.ItemsSource = null;
+            listaDatos.ItemsSource = datos;
+        }
+
+        private void Buscar_Click(object sender, RoutedEventArgs e)
+        {
+            int num;
+            if (int.TryParse(txtNumero.Text, out num))
+            {
+                int index = datos.IndexOf(num);
+
+                if (index >= 0)
+                    txtResultado.Text = "Encontrado en posición: " + index;
+                else
+                    txtResultado.Text = "No encontrado";
+            }
         }
     }
 }
