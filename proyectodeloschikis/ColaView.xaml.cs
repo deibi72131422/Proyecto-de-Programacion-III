0e50830526f16c338c0db1bd6d9eb87b2b9472fd
Matias <matiasruizdecourt75@gmail.com>
2026-03-22 14:59:24 -0400

    agregue colas me faltan listas enlazdas simples y dobles

--- proyectodeloschikis/ColaView.xaml.cs
+++ proyectodeloschikis/ColaView.xaml.cs
@@ -20,9 +20,71 @@ namespace proyectodeloschikis
     /// </summary>
     public partial class ColaView : UserControl
     {
+        Queue<string> cola = new Queue<string>();
+        int max = 10;
+
         public ColaView()
         {
             InitializeComponent();
+        }
+
+        // insertar
+        private void btnInsertar_Click(object sender, RoutedEventArgs e)
+        {
+            if (cola.Count < max)
+            {
+                cola.Enqueue(txtInsertar.Text);
+                listaDatos.Items.Add(txtInsertar.Text);
+                txtInsertar.Clear();
+            }
+        }
+
+        // quitar
+        private void btnQuitar_Click(object sender, RoutedEventArgs e)
+        {
+            if (cola.Count > 0)
+            {
+                cola.Dequeue();
+                listaDatos.Items.RemoveAt(0);
+            }
+        }
+
+        // limpiar
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            cola.Clear();
+            listaDatos.Items.Clear();
+        }
+
+        // esta vacia
+        private void btnVacia_Click(object sender, RoutedEventArgs e)
+        {
+            if (cola.Count == 0)
+                txtEstaVacia.Text = "Si";
+            else
+                txtEstaVacia.Text = "No";
+        }
+
+        // esta llena
+        private void btnLlena_Click(object sender, RoutedEventArgs e)
+        {
+            if (cola.Count == max)
+                txtEstaLlena.Text = "Si";
+            else
+                txtEstaLlena.Text = "No";
+        }
+
+        // tamaño maximo
+        private void btnTamano_Click(object sender, RoutedEventArgs e)
+        {
+            txtTamanoMaximo.Text = max.ToString();
+        }
+
+        // cima
+        private void btnCima_Click(object sender, RoutedEventArgs e)
+        {
+            if (cola.Count > 0)
+                txtCima.Text = cola.Peek();
         }
     }
 }
